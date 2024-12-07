b1532a07638f8f5f24fcebdbffbb6dd5cd8271ed
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-12-07 14:35:32 -0400

    ADD- More stores from the api

--- MyApp/Data/LocalData.cs
+++ MyApp/Data/LocalData.cs
@@ -8,11 +8,25 @@ namespace MyApp.Data
         public List<Store> Stores = new List<Store>
         {
             new Store(1,"Steam"),
-            new Store(4,"Amazon"),
-            new Store(5,"Gamestop"),
+            new Store(2,"Gamers Gate"),
+            new Store(3,"Green Man Gaming"),
+            new Store(7,"GOG"),
             new Store(8,"Origin"),
+            new Store(11,"Humble Store"),
+            new Store(13,"Uplay"),
+            new Store(15,"Fanatical"),
+            new Store(21,"WinGameStore"),
+            new Store(23,"GameBillet"),
+            new Store(24,"Voidu"),
             new Store(25,"Epic Games Store"),
-
+            new Store(27,"GamesPlanet"),
+            new Store(28,"GamesLoad"),
+            new Store(29,"2Game"),
+            new Store(30,"IndieGala"),
+            new Store(31,"Blizzard Shop"),
+            new Store(33,"DLGamer"),
+            new Store(34,"Noctre"),
+            new Store(35,"DreamGame"),
         };
     }
 }
