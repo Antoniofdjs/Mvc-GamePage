b2f66b7471b049e7bc79d313bec2244b601c4442
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-12-08 21:56:45 -0400

    UPDATE- Reviews atributes for meta and steam

--- MyApp/Models/Review.cs
+++ MyApp/Models/Review.cs
@@ -3,9 +3,10 @@ namespace MyApp.Models
 
     public class Review // Used for storing the steam and metacritic reviews
     {
-        public int MetacriticScore { get; set; }
-        public string MetaCriticLink { get; set; }
-        public int SteamRating { get; set; }
+        public int MetacriticScore { get; set; } = 0;
+        public string MetacriticLink { get; set; } = ""; // from api metacriticLink="/pc/bioshock-infinite"change it into https://www.metacritic.com{metacriticLink}
+        public int SteamRating { get; set; } = 0;
+        public string SteamLink { get; set; } = "";
 
         public Review() { }
 
