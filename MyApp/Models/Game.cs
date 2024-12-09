561103fd4748ec7f86b57787f30072a7c9759871
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-12-08 22:16:33 -0400

    UPDATE- added steamAppID sttribute

--- MyApp/Models/Game.cs
+++ MyApp/Models/Game.cs
@@ -3,6 +3,7 @@ namespace MyApp.Models
     public class Game
     {
         public string Title { get; set; }
+        public int SteamAppID { get; set; } = 0;
         public List<Deal> Deals { get; set; }
         public string Thumb { get; set; }
         public Review Review { get; set; }
