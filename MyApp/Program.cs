cd8fc90d180bf0eb5de690c1d4ca1a165637f7bd
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-12-10 15:12:08 -0400

    UPDATE- Load env files

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,6 +1,10 @@
+using DotNetEnv;
 using MyApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Load environment variables from the .env file
+Env.Load();
 
 // Register HttpClient in the DI container
 builder.Services.AddHttpClient();
