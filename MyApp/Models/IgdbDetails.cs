50a7fc6b13075f611dcc6d030cb004b2d8e2fdd7
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-12-13 18:52:31 -0400

    ADD- Igdb details model

--- MyApp/Models/IgdbDetails.cs
+++ MyApp/Models/IgdbDetails.cs
@@ -13,3 +13,21 @@
 
 
 // IndexGameVM.IgdbDetails.Videos   "Aqui saco los videos "
+using Microsoft.AspNetCore.SignalR;
+
+namespace MyApp.Models
+{
+
+    public class IgdbDetails
+    {
+        public string SlugTitle { get; set; } = "";
+        public string StoryLine { get; set; } = "";
+        public string Summary { get; set; } = "";
+        public int RatingCount { get; set; } = 0;
+        public string RatingLink { get; set; } = ""; // Complete igdb review link community #
+        public List<string>? ThemesGenres { get; set; } = null;
+        public List<string>? VideosLinks { get; set; } = null; // Complete youtube Video Link EMBED "https://www.youtube.com/embed/{videoId}"
+
+    }
+
+}
