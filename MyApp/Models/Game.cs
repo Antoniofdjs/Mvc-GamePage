d1a1810029d1afb9280766df005f7a1afe30d084
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-12-13 02:44:41 -0400

    UPDATE- Comments for the igdb attributes we need

--- MyApp/Models/Game.cs
+++ MyApp/Models/Game.cs
@@ -2,11 +2,28 @@ namespace MyApp.Models
 {
     public class Game
     {
+        //Cheap shark
         public string Title { get; set; }
         public int SteamAppID { get; set; } = 0;
         public List<Deal> Deals { get; set; }
         public string Thumb { get; set; }
         public Review Review { get; set; }
+
+        //IGDB
+        /*
+        If you search by title in games:
+        
+        ScreenShots foreing id
+        Videos---youtube id https://www.youtube.com/embed/{videoID} using iframe
+        Themes= Themes + Genres foreing id array
+        Summary string
+        StoryLine string
+        Rating Double
+
+        */
+
         public Game() { }
+
+
     }
 }
