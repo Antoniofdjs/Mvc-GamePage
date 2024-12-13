6e2a415a2e224540852fb0380de0cc2352afe451
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-12-13 02:45:04 -0400

    ADD- Igdb  detailscommented file template

--- MyApp/Models/IgdbDetails.cs
+++ MyApp/Models/IgdbDetails.cs
@@ -1,1 +1,15 @@
 //toda la data de IGDB
+//  ScreenShots foreing id
+//         Videos   youtube id https://www.youtube.com/embed/{videoID} using iframe
+//         Themes Themes + Genres foreing id array
+//         Summary string
+//         StoryLine string
+//         Rating Double
+
+
+// Create view Model for IndexGameVM
+//Game "CheapShark"
+//IgdbDetails
+
+
+// IndexGameVM.IgdbDetails.Videos   "Aqui saco los videos "
