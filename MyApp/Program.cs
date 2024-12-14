488a6483d525635238c75003e47507a5af4087b4
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-12-14 17:10:41 -0400

    ADD- Inserted Igdb service class to fecth api data.

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,27 +1,34 @@
 using DotNetEnv;
 using MyApp.Data;
 using MyApp.Models;
+using MyApp.Services.IGDB;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Load environment variables from the .env file
 Env.Load();
 
-// Register HttpClient in the DI container
 builder.Services.AddHttpClient();
+
+// Local db contains stores
+builder.Services.AddSingleton<LocalData>();
 
 // Register TwitchTokenManager as a Singleton, passing the HttpClient instance
 builder.Services.AddSingleton<TwitchTokenManager>(provider =>
 {
-// Get the HttpClient instance from the DI container
-var httpClient = provider.GetRequiredService<HttpClient>();
-
-// Return a new TwitchTokenManager instance with the HttpClient
-return new TwitchTokenManager(httpClient);
+    var httpClient = provider.GetRequiredService<HttpClient>();
+    return new TwitchTokenManager(httpClient);
 });
 
-// Local db contains stores
-builder.Services.AddSingleton<LocalData>();
+// IGDB api service class
+builder.Services.AddSingleton<Igdb>(provider =>
+{
+    var httpClient = provider.GetRequiredService<HttpClient>();
+    var twitchTokenManager = provider.GetRequiredService<TwitchTokenManager>();
+    var localData = provider.GetRequiredService<LocalData>();
+    return new Igdb(httpClient, twitchTokenManager, localData);
+});
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
