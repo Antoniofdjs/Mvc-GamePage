b30967facc072a43a8c51113f2301c10f0596e76
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-12-15 18:36:18 -0400

    UPDATE - Refactor to use IgdbGameDetails

--- MyApp/Models/ViewModels/IndexGameVM.cs
+++ MyApp/Models/ViewModels/IndexGameVM.cs
@@ -1,17 +1,17 @@
-
+using MyApp.Models.Igdb;
 namespace MyApp.Models.ViewModels
 {
     public class IndexGameVM
     {
         public Game? Game { get; set; } = null; // cheapShark only
-        public IgdbDetails? IgdbDetails { get; set; } = null;// Igdb Only
+        public IgdbGameDetails? IgdbDetails { get; set; } = null;// Igdb Only
 
         //Game
         //IgdbDetails
         public IndexGameVM()
         {
             Game = new Game();
-            IgdbDetails = new IgdbDetails();
+            IgdbDetails = new IgdbGameDetails();
         }
     }
 }
