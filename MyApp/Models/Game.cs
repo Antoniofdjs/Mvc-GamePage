2591bd54b828c5eda598569e6182c0a448a84a47
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-12-26 20:59:00 -0400

    UPDATE- New game model

--- MyApp/Models/Game.cs
+++ MyApp/Models/Game.cs
@@ -9,23 +9,6 @@ namespace MyApp.Models
         public int SteamAppID { get; set; } = 0;
         public string Thumb { get; set; } // url for image cheapShark
 
-        public List<MainDeal> Deals { get; set; }
-        public List<int> DealsIDs { get; set; }
-        public Review Review { get; set; }
-
-        //IGDB
-        /*
-        If you search by title in games:
-        
-        ScreenShots foreing id
-        Videos---youtube id https://www.youtube.com/embed/{videoID} using iframe
-        Themes= Themes + Genres foreing id array
-        Summary string
-        StoryLine string
-        Rating Double
-
-        */
-
         public Game() { }
 
         public Game(JObject gameInfo)
